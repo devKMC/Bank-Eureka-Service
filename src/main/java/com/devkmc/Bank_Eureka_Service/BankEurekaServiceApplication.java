b6e1cf97287f43ef24ce155366b8ac7aeeddf783
allcasa <allcasa@naver.com>
2024-11-01 13:23:03 +0900

    BankSystem

---
+++ src/main/java/com/devkmc/Bank_Eureka_Service/BankEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devkmc.Bank_Eureka_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkmc/Bank_Eureka_Service/BankEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkmc.Bank_Eureka_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
